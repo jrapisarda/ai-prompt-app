84a24e677aef0e39ca07c933cfc9f9db2dd14063
jrapisarda <jrapisarda@live.com>
2025-05-29 18:08:51 -0600

    Update app.py to dynamic port
    app.run(debug=True, host="0.0.0.0", port=port)

--- app.py
+++ app.py
@@ -170,4 +170,5 @@ def dashboard():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT",8080))
+    app.run(debug=True, host="0.0.0.0", port=port)
